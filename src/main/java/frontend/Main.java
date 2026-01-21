956cbfd2b46c55dfbd69237cb4292d9dbc492998
Martin Gou <martin0248159@gmail.com>
2026-01-21 16:57:15 +0100

    use lib-version VersionUtil in app

--- src/main/java/frontend/Main.java
+++ src/main/java/frontend/Main.java
@@ -1,12 +1,16 @@
 package frontend;
 
+// Import lib-version and use VersionUtil to print version at startup
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import tudelft.doda2025.team21.VersionUtil;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("lib-version version: " + VersionUtil.getVersion());
         SpringApplication.run(Main.class, args);
     }
 
